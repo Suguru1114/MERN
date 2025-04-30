eb514028f387461f9e061e3af45dbadf80e3e12f
Suguru Miyata <suguru1114@gmail.com>
2025-04-30 20:44:56 +1000

    Add backend configuration, models, and update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,21 @@
 import express from 'express';
+import dotenv from "dotenv";
+import {connectDB} from './config/db.js';
 
- const app = express();
+dotenv.config();
 
- app.get("/", (req, res) => {
-    res.send("Server is ready");
- });
+const app = express();
+
+app.get("/", (req, res) => {
+
+console.log(process.env.MANGO_URI);
+
+});
 
  app.listen(3000, () => {
+    connectDB();
     console.log("Server started at http://localhost:3000");
 
 });
+
+
