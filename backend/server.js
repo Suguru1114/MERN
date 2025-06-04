fe1f17a51ffe310a035b64ac3b3277587e0fa3f5
Suguru Miyata <suguru1114@gmail.com>
2025-06-05 00:09:02 +1000

    changing port

--- backend/server.js
+++ backend/server.js
@@ -2,23 +2,34 @@ import express from 'express';
 import dotenv from "dotenv";
 import {connectDB} from './config/db.js';
 
+
+
+
 import productRoute from './routes/product.route.js';
 
 dotenv.config();
 
+
+
+
 const app = express();
 const PORT = process.env.PORT  || 3000;   
 
+
+import cors from 'cors';
 
 
 app.use(express.json());//allows us to use json data in the body of the request(req.body)
 
 app.use("/api/products", productRoute); 
 
- app.listen(3000, () => {
+ app.listen(PORT, () => {
     connectDB();
-    console.log("Server started at http://localhost:3000");
+console.log(`Server started at http://localhost:${PORT}`);
 
 });
 
 
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
